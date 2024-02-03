7756e90562cedd6a2e56db575621fe5ba956b154
Swomp03 <robbie.yx.l@gmail.com>
2024-02-03 12:02:31 -0500

    Initial commit for this site

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GameSelect from "./components/GameSelect";
+
+function App() {
+  return (
+    <>
+      <GameSelect></GameSelect>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/GameSelect.tsx
@@ -0,0 +1,90 @@
+import {motion} from "framer-motion";
+
+import street_fighter_logo from "../images/SF6LogoNoShadow.png";
+import tekken_8_logo from "../images/Tekken8Logo.png";
+import guilty_gear_logo from "../images/GuiltyGearStriveLogo.png";
+
+import luke_start from "../images/LukeScreenShot.png"
+
+import "./componentStyles/GameSelect.css";
+
+const GameSelect = () => { 
+
+  const logoAnimation = (dampingAmount: Number, stiffnessAmount: Number, yVal = 50) => ({
+    hidden: {
+      opacity: 0,
+      y: yVal
+    },
+
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.1, type:"spring", damping: dampingAmount, stiffness: stiffnessAmount
+      }
+    }
+  });
+
+  
+
+  return (
+    <>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={logoAnimation(32, 650, -50)}
+      >
+        <h1>Hobbies: Fighting Games</h1>
+      </motion.div>
+
+
+      <div className="game-list">
+
+          <div>
+            <img id="luke-sf-start" src={luke_start} alt="Luke Start Image" />
+
+            <motion.div
+
+              initial="hidden"
+              whileInView="visible"
+              variants={logoAnimation(25, 500)}
+
+              whileHover={{scale: 1.15, transition:{duration:0.2}}}
+
+              id="sf-start"
+            >
+              <img id="sf-logo" src={street_fighter_logo} alt="Street Fighter 6 Logo" />
+            </motion.div>
+          </div>
+          
+
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={logoAnimation(17, 350)}
+            whileHover={{scale: 1.15, transition:{duration:0.2}}}
+            id="tekken-start"
+          >
+            <img id="tekken-logo" src={tekken_8_logo} alt="Tekken 8 Logo" />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={logoAnimation(13, 250)}
+            whileHover={{scale: 1.15, transition:{duration:0.2}}}
+            id="gg-start"
+          >
+            <img id="gg-logo" src={guilty_gear_logo} alt="Guilty Gear Strive Logo" />
+          </motion.div>
+
+      </div>
+
+      <div className="game-list" id="temp">
+        <img id="sf-logo" src={street_fighter_logo} alt="Street Fighter 6 Logo" />
+      </div>
+    </>
+  );
+};
+
+export default GameSelect;
