fc5ac84a2ac89a0146c9da87258996593c6a312b
Swomp03 <robbie.yx.l@gmail.com>
2024-02-17 21:23:46 -0500

    Added smoother animations to the Tekken section

--- src/components/TekkenSection.tsx
+++ src/components/TekkenSection.tsx
@@ -22,82 +22,167 @@ import "../components/componentStyles/TekkenSection.css"
 
 
 const TekkenSection = () => {
+
+  const container = {
+    hidden: { opacity: 1, scale: 1 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.05,
+        staggerChildren: 0.05
+      }
+    }
+  };
+  
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
+
+
+
+
   return (
     <>
-      <div id="tekken-section">
-        <img id="tekken-logo" src={TekkenLogo} alt="Tekken 8 Logo" />
+      <motion.ul
+        className="container"
+        variants={container}
+        initial="hidden"
+        whileInView="visible"
+      >
+        <div id="tekken-section">
+          <motion.li
+            className="item"
+            variants={item}
+          >
+            <img id="tekken-logo" src={TekkenLogo} alt="Tekken 8 Logo" />
+          </motion.li>
 
-        <div>      
-          <div className="leftside">
-            <img id="victor-main" src={VictorPNG} alt="Victor Chevalier" />
+          <div>
+            <div className="leftside">
+              
+              <div className="vertical-gallery">
 
-            <div className="vertical-gallery">
-              <img src={VictorGalleryOne} alt="Victor Gallery 1" />
-              <br />
-              <img src={VictorGalleryTwo} alt="Victor Gallery 2" />
-              <br />
-              <img src={VictorGalleryThree} alt="Victor Gallery 3" />
-            </div>
-          </div>
+                <motion.li
+                  className="item"
+                  variants={item}
+                >
+                  <img src={VictorGalleryOne} alt="Victor Gallery 1" />
+                </motion.li>
 
-          <div className="rightside">
-            <div className="carousel-container rightside">
-              <Carousel fade controls={false}>
-                <Carousel.Item>
-                  <img
-                    className="carousel-image d-block w-100"
-                    src={VictorSlideshowZero}
-                    alt="First slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="carousel-image d-block w-100"
-                    src={VictorSlideshowOne}
-                    alt="Second slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="carousel-image d-block w-100"
-                    src={VictorSlideshowTwo}
-                    alt="Third slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="carousel-image d-block w-100"
-                    src={VictorSlideshowThree}
-                    alt="Fourth slide"
-                  />
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img
-                    className="carousel-image d-block w-100"
-                    src={VictorSlideshowFour}
-                    alt="Fifth slide"
-                  />
-                </Carousel.Item>
-              </Carousel>
-            </div>
-            
-            <div className="sub-list">
-              <div className="left-sub-list">
-                <h2>Character:</h2>
-                <p>Victor Chevalier</p>
-                <h2>Type:</h2>
-                <p>Counterhit & Commital</p>
+                <br />
+
+                <motion.li
+                  className="item"
+                  variants={item}
+                >
+                  <img src={VictorGalleryTwo} alt="Victor Gallery 2" />
+                </motion.li>
+
+                <br />
+
+                <motion.li
+                  className="item"
+                  variants={item}
+                >
+                  <img src={VictorGalleryThree} alt="Victor Gallery 3" />
+                </motion.li>
+
               </div>
 
-              <div className="right-sub-list">
-                <h2>Highest Rank:</h2>
-                <img src={Tekken8Rank} alt="Tekken 8 Rank" />
-              </div>
+              <motion.li
+                className="item"
+                variants={item}
+                id="victor-main"
+              >
+                <img src={VictorPNG} alt="Victor Chevalier" />
+              </motion.li>
+
             </div>
-            
+
+            <div className="rightside">
+              <motion.li
+                variants={item}
+                className="item carousel-container rightside"
+              >
+
+                <Carousel fade controls={false} interval={2500}>
+                  <Carousel.Item>
+                    <img
+                      className="carousel-image d-block w-100"
+                      src={VictorSlideshowZero}
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="carousel-image d-block w-100"
+                      src={VictorSlideshowOne}
+                      alt="Second slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="carousel-image d-block w-100"
+                      src={VictorSlideshowTwo}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="carousel-image d-block w-100"
+                      src={VictorSlideshowThree}
+                      alt="Fourth slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="carousel-image d-block w-100"
+                      src={VictorSlideshowFour}
+                      alt="Fifth slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+
+              </motion.li>
+              
+              <div className="sub-list">
+                <div className="left-sub-list">
+                  <motion.li
+                    className="item"
+                    variants={item}
+                  >
+                    <h2>Character:</h2>
+                    <p>Victor Chevalier</p>
+                  </motion.li>
+
+                  <motion.li
+                    className="item"
+                    variants={item}
+                  >
+                    <h2>Type:</h2>
+                    <p>Counterhit & Commital</p>
+                  </motion.li>
+                </div>
+
+                <motion.li
+                  variants={item}
+                  className="item right-sub-list"
+                >
+                  <h2>Highest Rank:</h2>
+                  <img src={Tekken8Rank} alt="Tekken 8 Rank" />
+                </motion.li>
+              </div>
+              
+            </div>
           </div>
         </div>
-      </div>
+      </motion.ul>
     </>
   )
 }
