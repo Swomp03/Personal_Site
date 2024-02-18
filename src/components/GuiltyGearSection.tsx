dde8ed7d85bbdab332b413ef5d4cc7fcf2a7c5a3
Swomp03 <robbie.yx.l@gmail.com>
2024-02-18 15:38:05 -0500

    Guilty Gear Section

---
+++ src/components/GuiltyGearSection.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {motion} from "framer-motion";
+
+import MayPNG from "../images/MayGGS.png";
+import MaySlideshow0 from "../images/MayIntro.jpg";
+import MaySlideshow1 from "../images/MaySlideshow1.png";
+import MaySlideshow2 from "../images/MaySlideshow2.png";
+import MaySlideshow3 from "../images/MaySlideshow3.png";
+import MaySlideshow4 from "../images/MaySlideshow4.png";
+
+
+
+
+const GuiltyGearSection = () => {
+  return (
+    <div>GuiltyGearSection</div>
+  )
+}
+
+export default GuiltyGearSection
