9a5027c9dd584525ff2f0e9da7aa9fff2df3b226
Swomp03 <robbie.yx.l@gmail.com>
2024-03-31 15:18:36 -0400

    Adjusted header and header tags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ import "./componentStyles/Header.css"
 const Header = () => {
   return (
     <div id="site-header">
-        <a href="/"><h2>Home</h2></a>
-        <a href="/"><h2>Projects</h2></a>
-        <a href="/"><h2>Hobbies</h2></a>
+        <a href="/" id="home-button"><h3>Home</h3></a>
+        <a href="/" id="projects-button"><h3>Projects</h3></a>
+        <a href="/" id="hobbies-button"><h3>Hobbies</h3></a>
     </div>
   )
 }
