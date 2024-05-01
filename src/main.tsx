c8f322ef459ebc7856f7180a5524890e4a25e2ae
Swomp03 <robbie.yx.l@gmail.com>
2024-04-30 20:59:18 -0400

    Fixed the react router for modern day use

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.tsx'
 import './index.css'
 
+import Home from './components/Home.tsx'
+import GameSelect from './components/GameSelect.tsx'
+import Projects from './components/Projects.tsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home/>,
+    errorElement: <div>404 Not Found</div>
+  },
+  {
+    path:'/hobbies',
+    element: <GameSelect/>
+  },
+  {
+    path:'/projects',
+    element: <Projects/>
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
