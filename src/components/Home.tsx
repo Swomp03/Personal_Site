a9f0b88c65f3b1d9ea14a07c165f03e099a258a1
Swomp03 <robbie.yx.l@gmail.com>
2024-06-04 20:18:39 -0400

    Additions to site

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
+
+import "./componentStyles/Home.css"
+
+import rocketImage from '../images/rocket.png'
 
 const Home = () => {
   return (
     <>
       <Header></Header>
-      <div>Home</div>
+      <div>
+        <h2 className='home-h2'>Hi! I'm <b>Robert</b>.</h2>
+        <div id="home-image">
+          <img src={rocketImage} alt="rocket" />
+        </div>
+        <h2 className="home-subtitle-h2">A <b><i>Software Developer</i></b>.</h2>
+      </div>
+      <Footer></Footer>
     </>
   )
 }
