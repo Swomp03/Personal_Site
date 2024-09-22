6024cb01a9e3c6994a79d16600e5c6d2ba5b4373
Robert Liao <rliao29@uwo.ca>
2024-09-22 18:55:02 -0400

    Main Project animations

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,5 @@
-// import React from 'react'
+import {motion} from "framer-motion";
+
 import Header from './Header'
 import Footer from './Footer'
 
@@ -11,64 +12,119 @@ import P5Mask from "../images/P5Mask.png"
 import BootstrapIcon from "../images/BootstrapLogo.png"
 import ReactIcon from "../images/reactimg.png"
 
+import {useRef} from "react";
 
 const Projects = () => {
   const personaList = ['HTML', 'CSS', 'JavaScript', 'Git', 'Bootstrap'];
   const personalSiteList = ['React', 'TypeScript', 'Bootstrap', 'Vite', 'HTML', 'CSS', 'Git']
 
+  const mainProject_container = {
+    hidden: { opacity: 1, scale: 1 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.05,
+            staggerChildren: 0.05
+          }
+        }
+  }
+
+  const mainProject_item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
+  const mainProject_scrollRef = useRef(null);
+
   return (
     <>
       <Header></Header>
-      <h2 className='projects-h2'><b>Projects</b></h2>
-      <h3 className='projects-h3'>Web Development</h3>
-
-      <div className='dev-body'>
-        <SingleProject
-          title="Persona Social Link Guide"
-          projectBackground="project-background-1"
-
-          leftImg={HtmlCssJS}
-          leftAlt="HtmlCssJS Logo"
-          leftImgClass="htmlIcon"
-
-          rightImg={P5Mask}
-          rightAlt="Persona Mask"
-          rightImgClass="personaIcon"
-
-          explanation="A site that guides players through a mechanic in the video game series: Persona."
-          skills={personaList}
-          
-          link="https://swomp03.github.io/Persona_Project/"
-          webAlt="Persona Project Link"
-
-          gitHubLink="https://github.com/Swomp03/Persona_Project"
-          gitHubAlt="Persona Project GitHub Link"
+      
+      <motion.ul
+        ref={mainProject_scrollRef}
+        className="mainProject_container"
+        variants={mainProject_container}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once: true}}
+      >
+        <motion.div
+          className="mainProject_item"
+          variants={mainProject_item}
         >
-        </SingleProject>
-
-        <SingleProject
-          title="Personal Site"
-          projectBackground="project-background-2"
-
-          leftImg={BootstrapIcon}
-          leftAlt="Bootstrap Icon"
-          leftImgClass="bootstrapIcon"
-
-          rightImg={ReactIcon}
-          rightAlt="React Icon"
-          rightImgClass="reactIcon"
-
-          explanation="Personal site for myself."
-          skills={personalSiteList}
-
-          link="/"
-          webAlt="Personal Site Link"
-
-          gitHubLink="https://github.com/Swomp03/Personal_Site"
-          gitHubAlt="Personal Site GitHub Link"
+          <h2 className='projects-h2'><b>Projects</b></h2>
+        </motion.div>
+        
+        <motion.div
+          className="mainProject_item"
+          variants={mainProject_item}
         >
-        </SingleProject>
-      </div>
+          <h3 className='projects-h3'>Web Development</h3>
+        </motion.div>
+      
+
+        <div className='dev-body'>
+          <motion.div
+            className="mainProject_item"
+            variants={mainProject_item}
+          >
+            <SingleProject
+              title="Persona Social Link Guide"
+              projectBackground="project-background-1"
+
+              leftImg={HtmlCssJS}
+              leftAlt="HtmlCssJS Logo"
+              leftImgClass="htmlIcon"
+
+              rightImg={P5Mask}
+              rightAlt="Persona Mask"
+              rightImgClass="personaIcon"
+
+              explanation="A site that guides players through a mechanic in the video game series: Persona."
+              skills={personaList}
+              
+              link="https://swomp03.github.io/Persona_Project/"
+              webAlt="Persona Project Link"
+
+              gitHubLink="https://github.com/Swomp03/Persona_Project"
+              gitHubAlt="Persona Project GitHub Link"
+            >
+            </SingleProject>
+          </motion.div>
+
+          <motion.div
+            className="mainProject_item"
+            variants={mainProject_item}
+          >
+            <SingleProject
+              title="Personal Site"
+              projectBackground="project-background-2"
+
+              leftImg={BootstrapIcon}
+              leftAlt="Bootstrap Icon"
+              leftImgClass="bootstrapIcon"
+
+              rightImg={ReactIcon}
+              rightAlt="React Icon"
+              rightImgClass="reactIcon"
+
+              explanation="Personal site for myself."
+              skills={personalSiteList}
+
+              link="/"
+              webAlt="Personal Site Link"
+
+              gitHubLink="https://github.com/Swomp03/Personal_Site"
+              gitHubAlt="Personal Site GitHub Link"
+            >
+            </SingleProject>
+          </motion.div>
+        </div>
+      </motion.ul>
       <Footer></Footer>
     </>
   )
--- src/components/SingleProject.tsx
+++ src/components/SingleProject.tsx
@@ -1,39 +1,73 @@
+import {motion} from "framer-motion";
+
 import "./componentStyles/SingleProject.css"
 import webIcon from "../images/internetIcon.png"
 import gitHubAlt from "../images/GithubIconReverse.png"
+
+import {useRef} from "react";
 
 const SingleProject = (props: any) => {
 
     const skills = props.skills;
     
+    const project_container = {
+        hidden: { opacity: 1, scale: 1 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.05,
+            staggerChildren: 0.05
+          }
+        }
+    };
+
+    const project_item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1
+        }
+    }
+
+    const project_scrollRef = useRef(null);
 
     return(
-        <div className={`project-div ${props.projectBackground}`}>
-            <div className="project-header">
-                <img className= {`project-icon ${props.leftImgClass}`}  src={props.leftImg} alt={props.leftAlt} />
-                <h2 className="project-title"><b>{props.title}</b></h2>
-                <img className={`project-icon ${props.rightImgClass}`} src={props.rightImg} alt={props.rightAlt} />
-            </div>
-            
+        <>
+            <motion.ul
+                ref={project_scrollRef}
+                variants={project_container}
+                initial="hidden"
+                whileInView="visible"
+            >
+                <div className={`project-div ${props.projectBackground}`}>
+                    <div className="project-header">
+                        <img className= {`project-icon ${props.leftImgClass}`}  src={props.leftImg} alt={props.leftAlt} />
+                        <h2 className="project-title"><b>{props.title}</b></h2>
+                        <img className={`project-icon ${props.rightImgClass}`} src={props.rightImg} alt={props.rightAlt} />
+                    </div>
+                    
 
-            <h2 className="explanation">{props.explanation}</h2>
+                    <h2 className="explanation">{props.explanation}</h2>
 
-            <ul className="skill-list">
-                {skills.map((skill: string) => <li className="individual-skill">{skill}</li>)}
-            </ul>
-            
-            <div className="links">
-                <a className="webIcon" href={props.link} target="blank">
-                    <img src={webIcon} alt={props.webAlt}/>
-                    <p>Website</p>
-                </a>
+                    <ul className="skill-list">
+                        {skills.map((skill: string) => <li className="individual-skill">{skill}</li>)}
+                    </ul>
+                    
+                    <div className="links">
+                        <a className="webIcon" href={props.link} target="blank">
+                            <img src={webIcon} alt={props.webAlt}/>
+                            <p>Website</p>
+                        </a>
 
-                <a className="gitHubIcon" href={props.gitHubLink} target="blank">
-                    <img src={gitHubAlt} alt={props.gitHubAlt}/>
-                    <p>GitHub</p>
-                </a>
-            </div>
-        </div>
+                        <a className="gitHubIcon" href={props.gitHubLink} target="blank">
+                            <img src={gitHubAlt} alt={props.gitHubAlt}/>
+                            <p>GitHub</p>
+                        </a>
+                    </div>
+                </div>
+            </motion.ul>
+        </>
     );
 }
 
